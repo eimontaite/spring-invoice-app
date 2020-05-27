458a53085f66c3118d4f612f28ed7dd2aa5af37f
Aiste <eimontaite.aiste@gmail.com>
2020-05-27 18:47:07 +0300

    Add ContactDetails repository

---
+++ src/main/java/aiste/invoices/repositories/ContactDetailsRepository.java
@@ -0,0 +1,8 @@
+package aiste.invoices.repositories;
+
+import aiste.invoices.models.ContactDetails;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ContactDetailsRepository extends CrudRepository<ContactDetails, Long> {
+
+}
