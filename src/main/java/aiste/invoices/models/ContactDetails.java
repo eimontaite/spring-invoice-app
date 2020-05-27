8751416a35372c1b19d7a6b80f0bc9201fbac98f
Aiste <eimontaite.aiste@gmail.com>
2020-05-27 18:45:45 +0300

    Add ContactDetails model

---
+++ src/main/java/aiste/invoices/models/ContactDetails.java
@@ -0,0 +1,97 @@
+package aiste.invoices.models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "contact_details")
+public class ContactDetails {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Long id;
+
+	@Column(name = "name")
+	private String name;
+
+	@Column(name = "personal_number")
+	private Long personalNumber;
+
+	@Column(name = "business_licence_no")
+	private String businessLicenceNo;
+
+	@Column(name = "address")
+	private String address;
+
+	@Column(name = "phone")
+	private String phone;
+
+	@Column(name = "bank_account")
+	private String bankAccount;
+
+	@Column(name = "bank")
+	private String bank;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Long getPersonalNumber() {
+		return personalNumber;
+	}
+
+	public void setPersonalNumber(Long personalNumber) {
+		this.personalNumber = personalNumber;
+	}
+
+	public String getBusinessLicenceNo() {
+		return businessLicenceNo;
+	}
+
+	public void setBusinessLicenceNo(String businessLicenceNo) {
+		this.businessLicenceNo = businessLicenceNo;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getBankAccount() {
+		return bankAccount;
+	}
+
+	public void setBankAccount(String bankAccount) {
+		this.bankAccount = bankAccount;
+	}
+
+	public String getBank() {
+		return bank;
+	}
+
+	public void setBank(String bank) {
+		this.bank = bank;
+	}
+}
